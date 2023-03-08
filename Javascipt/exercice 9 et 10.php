c0e9994949e854e791df511b2fd87504f4122024
LeducJean <jean.leduc80@gmail.com>
2023-03-08 17:27:32 +0100

    ajout des exercices 9 et 10

---
+++ Javascipt/exercice 9 et 10.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Exercice Test</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div id="test">Coucou</div>
+        <div name="toto">Coucou2</div>
+        <div>Coucou3</div>
+        <div class="titi">Coucou</div>
+        <div id="div1">Je suis La Div Num 1</div>
+    </body>
+    <script src="yyy.js"></script>
+</html>
---
+++ Javascipt/yyy.js
@@ -0,0 +1,41 @@
+function test() {
+  alert("Je vais modifier ma div !");
+  var i = document.getElementById("test");
+  i.innerHTML=("tesdt");
+  
+}
+
+
+
+function mafunction(){
+  //exo 9.1
+  var Elements = document.getElementsByName("toto");
+  Elements.forEach(element => {
+      element.innerHTML = "9.1";
+  });
+
+
+
+    //exo 9.2
+    Elements = document.getElementsByTagName("SPAN");
+    for(i=0;i<Elements.length;i++){
+        Elements[i].innerHTML = "9.2";
+    }
+
+    //exo 9.3
+    Elements = document.getElementsByClassName("titi");
+    for(i=0;i<Elements.length;i++){
+        Elements[i].innerHTML = "9.3";
+    }
+  }
+//exo 10
+function sapristi(){
+  var MaDiv = document.getElementById("div1");
+  MaDiv.addEventListener("click", UneProcedureQuiChangeLetext);
+  function UneProcedureQuiChangeLetext(evenement){
+    evenement.target.className = "rouge";
+  }
+}
+test();
+mafunction();
+sapristi();
